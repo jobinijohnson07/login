6410634b8606c4bd4ec139f4f007c21cb61acf17
manovijay1997 <jobinijohnson981@gmail.com>
2020-03-16 16:55:32 +0530

    Updates

---
+++ precache-manifest.cde29557d705807f306b01c8ba725430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794f7326203cae6d80ea47a0da644ee2",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "ecafc6e22f42f93999cf",
+    "url": "/login/static/css/main.fa8c2d95.chunk.css"
+  },
+  {
+    "revision": "4bae9e7e3fdcabc0c2e2",
+    "url": "/login/static/js/2.61117b98.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/login/static/js/2.61117b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecafc6e22f42f93999cf",
+    "url": "/login/static/js/main.86b18f11.chunk.js"
+  },
+  {
+    "revision": "89f8cced6e0b345ff102",
+    "url": "/login/static/js/runtime-main.80a8439a.js"
+  }
+]);
